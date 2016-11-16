dbd029608092598a262bfee37fc8dae2c1d89962
William Madden <madden.william.d@gmail.com>
2016-11-16 00:55:44 -0800

    building construction adding prototype methods

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 'use strict';
-//lets make our objects!!!
+// Lets make a Constructor robot for them there objects
+function SalmonStore (name, min, max, avgSale) {
+  this.name = name;
+  this.minHourlyCustos = min;
+  this.maxHourlyCustos = max;
+  this.avgPurchaseSize = avgSale;
+  this.dailyTotal = 0;
+  this.hours = 15;
+  this.storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ];
+
+}
+
+SalmonStore.prototype.setCustosPerHr = function () {
+  var custoEstimate;
+  var min = Math.ceil(this.minHourlyCustos);
+  var max = Math.floor(this.maxHourlyCustos);
+  custoEstimate = Math.floor(Math.random() * (max - min)) + min;
+  this.custosPerHr = custoEstimate;
+};
+
+SalmonStore.prototype.populateSalesData = function (){
+  var salesthathour;
+  this.salesRecord = [];
+  this.dailyTotal = 0;
+  this.salesRecord.push(this.name);
+  for ( var i = 0 ; i < this.storeHours.length; i ++){
+    this.setCustosPerHr();
+    salesthathour = this.custosPerHr * this.avgPurchaseSize;
+    this.salesRecord[i] = ' ' + this.storeHours[i - 1] + ': ' + Math.ceil(salesthathour) + ' cookies';
+    this.dailyTotal += salesthathour;
+  };
+  this.dailyTotal = Math.ceil(this.dailyTotal);
+  this.salesRecord.push('Total: ' + this.dailyTotal);
+};
+
 var pike  = {
   name : '1st and Pike',
   minHourlyCustos : 23,
